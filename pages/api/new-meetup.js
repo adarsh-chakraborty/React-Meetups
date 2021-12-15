f2f800369bd089b8ac07634866d4171e390cb41d
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-12-15 22:11:18 +0530

    Add new meetup handler added

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,23 +1,23 @@
 import { MongoClient } from 'mongodb';
 
 async function handler(req, res) {
-  if (req.method != 'POST') return;
+  if (req.method === 'POST') {
+    const { title, image, address, description } = req.body;
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    const db = client.db();
 
-  const { title, image, address, description } = req.body;
-  const client = await MongoClient.connect(process.env.MONGODB_URI);
-  const db = client.db();
+    const meetupsCollection = db.collection('meetups');
 
-  const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne({
+      title,
+      image,
+      address,
+      description
+    });
 
-  const result = await meetupsCollection.insertOne({
-    title,
-    image,
-    address,
-    description
-  });
-
-  client.close();
-  res.status(201).json({ message: 'Meetup Inserted' });
+    client.close();
+    res.status(201).json({ message: 'Meetup Inserted' });
+  }
 }
 
 export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,21 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { useRouter } from 'next/router';
 
 function NewMeetupPage() {
-  const meetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+  const meetupHandler = async (enteredMeetupData) => {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+    // router.replace('/'); // Disables back button.
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={meetupHandler} />;
